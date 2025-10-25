24e0f6b1cfaacf63153a82c7649de3f9213c693c
badoniekansh83-afk <badoniekansh83@gmail.com>
2025-10-25 10:08:49 +0530

    Add files via upload

---
+++ Day29/day29.1.cpp
@@ -0,0 +1,20 @@
+//Q57: Find the sum of array elements.
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, sum = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i]; // Add each element to sum
+    }
+
+    printf("Sum of array elements = %d\n", sum);
+
+    return 0;
+}
