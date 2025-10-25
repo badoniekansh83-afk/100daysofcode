92341e66bf1bd37ec4aec2b9720cc8779c9039a4
badoniekansh83-afk <badoniekansh83@gmail.com>
2025-10-25 11:16:38 +0530

    Add files via upload

---
+++ Day19/day19.2.c
@@ -0,0 +1,19 @@
+// Q38: Write a program to find the sum of digits of a number.
+
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        sum += num % 10;  // Add last digit
+        num /= 10;        // Remove last digit
+    }
+
+    printf("Sum of digits = %d\n", sum);
+
+    return 0;
+}
